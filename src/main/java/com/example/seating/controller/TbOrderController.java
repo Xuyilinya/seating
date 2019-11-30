ad0eb46bb104436b678acaef06c1481d50a23d46
1185733657@qq.com <Xyl520++>
2019-11-30 22:45:36 +0800

    【更新】修改暂离成功提示；调整暂离时间

--- src/main/java/com/example/seating/controller/TbOrderController.java
+++ src/main/java/com/example/seating/controller/TbOrderController.java
@@ -171,7 +171,7 @@ public class TbOrderController {
             TbOrder order = orderService.getById(orderId);
 
             // 在开始时间十五分钟前可取消
-            if (Integer.valueOf(order.getStartTime()) - LocalDateTime.now().getHour() == 1 && LocalDateTime.now().getMinute() >= 15 ) {
+            if (Integer.valueOf(order.getStartTime()) - LocalDateTime.now().getHour() == 1 && LocalDateTime.now().getMinute() >= 45 ) {
                 return ReturnUtils.Failure("请在开始前十五分钟取消");
             }else {
                 TbSeat seat = seatService.getById(order.getSeatId());
