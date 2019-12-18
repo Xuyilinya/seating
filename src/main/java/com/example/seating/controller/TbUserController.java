f7efee6f767c13f90b4efcf473281979785db482
Administrator <1185733657@qq.com>
2019-12-18 15:55:36 +0800

    【修改】修改状态管理

--- src/main/java/com/example/seating/controller/TbUserController.java
+++ src/main/java/com/example/seating/controller/TbUserController.java
@@ -2,6 +2,7 @@ package com.example.seating.controller;
 
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.example.seating.entity.TbUser;
 import com.example.seating.service.ITbUserService;
 import com.example.seating.utils.ReturnUtils;
@@ -9,6 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.time.LocalDateTime;
 
 /**
  * 用户模块
@@ -24,14 +26,45 @@ public class TbUserController {
     private ITbUserService userService;
 
     /**
+     * 用户列表分页
+     * @param current
+     * @param size
+     * @return
+     */
+    @GetMapping(value = "/page")
+    public Object page(@RequestParam int current,@RequestParam int size){
+        try {
+            return ReturnUtils.Success(userService.page(new Page<>(current,size)));
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return ReturnUtils.Failure();
+        }
+    }
+
+    /**
+     * 删除用户
+     * @param id
+     * @return
+     */
+    @DeleteMapping(value = "/remove/{id}")
+    public Object remove(@PathVariable("id")String id){
+        try {
+            return ReturnUtils.Success(userService.removeById(id));
+        }catch (Exception e){
+            log.error(e.getMessage());
+            return ReturnUtils.Failure();
+        }
+    }
+
+    /**
      * 保存用户
      * @return
      */
     @PutMapping(value = "/save")
     public Object save(@RequestBody TbUser user){
         try {
-            userService.save(user);
-            return ReturnUtils.Success(user.getUserId());
+            user.setCreateTime(LocalDateTime.now());
+            return ReturnUtils.Success(userService.save(user));
         }catch (Exception e){
             log.error(e.getMessage(),e);
             return ReturnUtils.Failure();
@@ -52,9 +85,6 @@ public class TbUserController {
             if (user == null) {
                 return ReturnUtils.Failure("请输入正确的图书证号码");
             }
-            user.setMobile(login.getMobile());
-            user.setName(login.getName());
-            userService.updateById(user);
 
             // 返回用户编号
             return ReturnUtils.Success(user.getUserId());
